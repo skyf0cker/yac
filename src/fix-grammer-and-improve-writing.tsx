a9c515a4b7cb67d12585daf159635884af8766b1
patricklamar <dev@vophan.day>
2024-03-02 14:59:21 +0800

    feat: improved prompt

--- src/fix-grammer-and-improve-writing.tsx
+++ src/fix-grammer-and-improve-writing.tsx
@@ -1,6 +1,6 @@
 import { Action, ActionPanel, Detail, LaunchProps, Toast, getSelectedText, preferences, showToast } from "@raycast/api";
 import { ChatOpenAI } from "langchain/chat_models/openai";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 const model = new ChatOpenAI({
     temperature: 0.1,
@@ -10,11 +10,13 @@ const model = new ChatOpenAI({
     streaming: true,
     configuration: {
         baseURL: preferences.baseURL.value as string,
-    }
+    },
 });
 
-function ImproveWritingComponent(props: LaunchProps<{ arguments: { content: string; }}>) {
-    const [improved, setImproved] = useState('');
+function ImproveWritingComponent(props: LaunchProps<{ arguments: { content: string } }>) {
+    const targetLang = "English";
+
+    const [improved, setImproved] = useState("");
     const [loading, setLoading] = useState(false);
     useEffect(() => {
         async function improveWriting() {
@@ -24,13 +26,20 @@ function ImproveWritingComponent(props: LaunchProps<{ arguments: { content: stri
                     text = await getSelectedText();
                 }
 
-                setLoading(true)
-                const response = await model.stream(`Please improve the grammar and authenticity of the following sentence, Return only the improved sentence without any explanations, tags, or quotes. For example, if given "I am good boys", you should return "I am a good boy. Now improve: ${text}"`);
+                setLoading(true);
+                const response = await model.stream(`
+                    Please improve the grammar and authenticity of the following sentence, 
+                    Return only the improved sentence without any explanations, tags, or quotes. 
+                    If what i given is not ${targetLang}, translate it into ${targetLang} at first. 
+                    For example, 
+                    if given "I am good boys", you should return "I am a good boy".
+                    if given "我是一个好男孩", you should return "I am a good boy". 
+                    Now improve: ${text}"`);
                 for await (const line of response) {
-                    console.log(line.content.toString())
+                    console.log(line.content.toString());
                     setImproved((prev) => prev + line.content.toString());
                 }
-                setLoading(false)
+                setLoading(false);
             } catch (error) {
                 await showToast({
                     style: Toast.Style.Failure,
@@ -44,12 +53,15 @@ function ImproveWritingComponent(props: LaunchProps<{ arguments: { content: stri
     }, []);
 
     return (
-        <Detail isLoading={loading} markdown={improved} actions={
-            <ActionPanel title="#1 in raycast/extensions">
-                <Action.CopyToClipboard title="Copy to Clipboard" content={improved} />
-            </ActionPanel>
-        }></Detail>
-    )
+        <Detail
+            isLoading={loading}
+            markdown={improved}
+            actions={
+                <ActionPanel title="#1 in raycast/extensions">
+                    <Action.CopyToClipboard title="Copy to Clipboard" content={improved} />
+                </ActionPanel>
+            }></Detail>
+    );
 }
 
 export default ImproveWritingComponent;
